d6d0ec41a7b71d1494aca20e950f8eaff9e6d5bc
Oleksandra Zakharova <oleksandraozakharova@gmail.com>
2025-03-13 20:39:35 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -26,11 +26,12 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/prop-types": 0,
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
